359476a4ba0f4331241c3e960af2c84b82a2216d
Antonio Saracino <antonio.saracino83@gmail.com>
2024-10-19 18:18:16 +0200

    FlatList

--- RNIntro/App.js
+++ RNIntro/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View, ScrollView } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View, ScrollView, FlatList } from "react-native";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -12,7 +12,7 @@ export default function App() {
   function addGoalHandler() {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      enteredGoalText,
+      {text: enteredGoalText, id: Math.random().toString()},
     ]);
   }
 
@@ -27,13 +27,21 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <ScrollView alwaysBounceVertical={false}>
-          {courseGoals.map((goal) => (
-            <View style={styles.goalItem} key={goal}>
-              <Text style={styles.goalText} >{goal}</Text>
-            </View>
-          ))}
-        </ScrollView>
+        <FlatList 
+          data={courseGoals} 
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            )
+        }} 
+        keyExtractor={(item, index) => {
+          return item.id
+        }}
+        alwaysBounceVertical={false} 
+
+        />
       </View>
 
     </View>
