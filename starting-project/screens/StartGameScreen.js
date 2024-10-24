aa3ee288e5524c190340f6de16e692fc7f0f4179
Antonio Saracino <104855075+braindome@users.noreply.github.com>
2024-10-24 12:23:13 +0200

    section 4

--- starting-project/screens/StartGameScreen.js
+++ starting-project/screens/StartGameScreen.js
@@ -2,13 +2,6 @@ import {Button, TextInput, View} from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
-    const myRef = useRef(null);
-
-    const handlePress = () => {
-        if (myRef.current) {
-            console.log('Ref is not null');
-        }
-    };
     return (
         <View>
             <TextInput />
