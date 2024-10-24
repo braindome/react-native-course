0148b67979acc520f3ff8f147d1c1610fc1292a7
Antonio Saracino <antonio.saracino83@gmail.com>
2024-10-24 13:30:55 +0200

    Section four - Styling buttons

--- section-four/components/PrimaryButton.js
+++ section-four/components/PrimaryButton.js
@@ -1,11 +1,48 @@
-import {Text, View} from "react-native";
+import {Text, View, Pressable, StyleSheet} from "react-native";
 
 function PrimaryButton({ children }) {
+
+    function pressHandler() {
+        console.log("Pressed!");
+    }
+
     return (
-        <View>
-            <Text>{children}</Text>
-        </View>
+    <View style={styles.buttonOuterContainer}>
+        <Pressable
+            style={({pressed}) =>
+                pressed
+                    ? [styles.buttonInnerContainer, styles.pressed]
+                    : styles.buttonInnerContainer
+            }
+            onPress={pressHandler}
+            android_ripple={{color: "#550229"}}
+        >
+            <Text style={styles.buttonText}>{children}</Text>
+        </Pressable>
+    </View>
     )
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: "hidden"
+    },
+    buttonInnerContainer: {
+        backgroundColor: "#72063c",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2
+    },
+    buttonText: {
+        color: "white",
+        textAlign: "center"
+    },
+    pressed: {
+        opacity: 0.75
+    }
+
+})
