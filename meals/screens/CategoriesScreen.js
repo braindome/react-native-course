464b2fc553737cbcf45556410f505755dd879ae9
Antonio Saracino <antonio.saracino83@gmail.com>
2024-10-29 12:54:18 +0100

    Navigation between screens, passing data

--- meals/screens/CategoriesScreen.js
+++ meals/screens/CategoriesScreen.js
@@ -2,19 +2,32 @@ import { FlatList } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
-function renderCategoryItem(itemData) {
-    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />;
-}
 
-function CategoriesScreen() {
-  return (
-    <FlatList 
-    data={CATEGORIES} 
-    keyExtractor={(item) => item.id} 
-    renderItem={renderCategoryItem}
-    numColumns={2}
-    />
-  );
+
+function CategoriesScreen({ navigation }) {
+
+    function renderCategoryItem(itemData) {
+        function pressHandler() {
+            navigation.navigate("MealsOverview", {
+                categoryId: itemData.item.id
+            });
+        }
+
+        return <CategoryGridTile
+            title={itemData.item.title}
+            color={itemData.item.color}
+            onPress={pressHandler}
+        />;
+    }
+
+    return (
+        <FlatList
+            data={CATEGORIES}
+            keyExtractor={(item) => item.id}
+            renderItem={renderCategoryItem}
+            numColumns={2}
+        />
+    );
 }
 
 export default CategoriesScreen;
