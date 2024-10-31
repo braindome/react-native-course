dc3164f89f240c32058e9c80253321987296356f
Antonio Saracino <104855075+braindome@users.noreply.github.com>
2024-10-31 23:52:12 +0100

    Nested navigation

--- meals/App.js
+++ meals/App.js
@@ -1,20 +1,76 @@
-import { StyleSheet } from 'react-native';
+import {Button, StyleSheet, Text} from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { StatusBar } from 'expo-status-bar';
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer  } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import MealDetailScreen from "./screens/MealDetailScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
+import {Ionicons} from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+
+function DrawerNavigator() {
+    return (
+        <Drawer.Navigator
+            screenOptions={{
+                headerStyle: { backgroundColor: '#351401' },
+                headerTintColor: 'white',
+                sceneContainerStyle: { backgroundColor: '#3f2f25' },
+                drawerContentStyle: { backgroundColor: '#3f2f25' },
+                drawerInactiveTintColor: 'white',
+                drawerActiveTintColor: "#351401",
+                drawerActiveBackgroundColor: "#e4baa1"
+            }}
+        >
+            <Drawer.Screen
+                name="Categories"
+                component={CategoriesScreen}
+                options={{
+                    title: 'All Categories',
+                    drawerIcon: ({color, size}) => (
+                        <Ionicons  name={"star"} color={color} size={size}/>
+                    ),
+                }}
+            />
+            <Drawer.Screen name="Favorites" component={FavoritesScreen} options={{
+                drawerIcon: ({color, size}) => (
+                    <Ionicons  name={"list"} color={color} size={size}/>
+                ),
+            }} />
+        </Drawer.Navigator>
+    );
+}
 
 export default function App() {
   return (
         <>
-            <StatusBar style="dark" />
+            <StatusBar style="light" />
             <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+                <Stack.Navigator screenOptions={{
+                    headerStyle: { backgroundColor: "#351401" },
+                    headerTintColor: "white",
+                    contentStyle: { backgroundColor: "#3f2f25" },
+                }}
+                >
+                    <Stack.Screen
+                        name="Drawer"
+                        component={DrawerNavigator}
+                        options={{
+                            headerShown: false
+                        }}
+                    />
                     <Stack.Screen name={"MealsOverview"} component={MealsOverviewScreen} />
+                    <Stack.Screen
+                        name="MealDetail"
+                        component={MealDetailScreen}
+                        options={{
+                            title: 'About the Meal',
+                        }}
+                    />
                 </Stack.Navigator>
             </NavigationContainer>
         </>
--- meals/components/MealItem.js
+++ meals/components/MealItem.js
@@ -1,22 +1,34 @@
 import {Text, View, Pressable, Image, StyleSheet, Platform} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import MealDetails from "./MealDetails";
 
-function MealItem({title, imageUrl, duration, complexity, affordability}) {
+function MealItem({id, title, imageUrl, duration, complexity, affordability}) {
+    const navigation = useNavigation();
+
+    function selectMealItemHandler() {
+        navigation.navigate("MealDetail", {
+            mealId: id
+        });
+    }
+
     return (
         <View style={styles.mealItem}>
             <Pressable
                 android_ripple={{color: "#ccc"}}
                 style={({pressed}) => (pressed ? styles.buttonPressed : null)}
+                onPress={selectMealItemHandler}
             >
                 <View style={styles.innerContainer}>
                     <View>
                         <Image source={{uri: imageUrl}} style={styles.image} />
                         <Text style={styles.title}>{title}</Text>
                     </View>
-                    <View style={styles.details}>
-                        <Text style={styles.detailIem}>{duration}m</Text>
-                        <Text style={styles.detailIem}>{complexity.toUpperCase()}</Text>
-                        <Text style={styles.detailIem}>{affordability.toUpperCase()}</Text>
-                    </View>
+                    <MealDetails
+                        duration={duration}
+                        affordability={affordability}
+                        complexity={complexity}
+                    />
+
                 </View>
 
             </Pressable>
@@ -47,16 +59,6 @@ const styles = StyleSheet.create({
         textAlign: "center",
         fontSize: 18,
         margin: 8
-    },
-    details: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 8
-    },
-    detailIem: {
-        marginHorizontal: 4,
-        fontSize: 12
     },
     innerContainer: {
         borderRadius: 8,
